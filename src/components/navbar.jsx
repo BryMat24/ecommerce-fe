fb1b0d1dd29d1c6f06510340befcdc9792ae36b6
BebekJK <kevinjk2802@gmail.com>
2024-02-19 15:50:36 +0800

    first commit

---
+++ src/components/navbar.jsx
@@ -0,0 +1,21 @@
+import {
+    NavigationMenu,
+    NavigationMenuContent,
+    NavigationMenuIndicator,
+    NavigationMenuItem,
+    NavigationMenuLink,
+    NavigationMenuList,
+    NavigationMenuTrigger,
+    NavigationMenuViewport,
+} from "@/components/ui/navigation-menu"
+
+export default function Navbar(){
+    return (
+        <div>
+            
+        </div>
+        // <NavigationMenu>
+            
+        // </NavigationMenu>
+    )    
+}
