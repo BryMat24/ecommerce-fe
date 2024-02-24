54212215882d02c359764046e76f98b653d84a6e
BebekJK <kevinjk2802@gmail.com>
2024-02-25 01:09:28 +0800

    feat: fix navbar navigation

--- src/components/navigation/navbar.jsx
+++ src/components/navigation/navbar.jsx
@@ -8,6 +8,45 @@ import { LuTruck } from "react-icons/lu";
 import { FiUser } from "react-icons/fi";
 import { IoSettingsOutline } from "react-icons/io5";
 
+
+const Routes = [
+    {
+        name: "Home",
+        icon: <AiOutlineHome className="text-2xl mr-4"/>,
+        path: "/"
+    },
+    {
+        name: "Category",
+        icon: <BiCategory className="text-2xl mr-4"/>,
+        path: "/explore"
+    },
+    {
+        name: "Order",
+        icon: <LuTruck className="text-2xl mr-4"/>,
+        path: "/order"
+    },
+    {
+        name: "Login",
+        icon: <FiUser className="text-2xl mr-4"/>,
+        path: "/login"
+    },
+    {
+        name: "Settings",
+        icon: <IoSettingsOutline className="text-2xl mr-4"/>,
+        path: "/settings"
+    }
+]
+
+const getNavigation = (route) => {
+    return(
+        <Button asChild className="py-6 px-2 my-1 align-left justify-start hover:bg-black hover:text-white" variant="ghost">
+            <Link href={route.path} className="">
+                {route.icon}
+                <div>{route.name}</div>
+            </Link>
+        </Button>
+    )
+}
 export default function Navbar() {
   return (
     <nav className="flex flex-col justify-between h-[calc(70vh-3.5rem)]">
@@ -15,39 +54,10 @@ export default function Navbar() {
             <h1 className="text-3xl font-bold">Explore</h1>
         </div>
         <div className="flex flex-col pb-16">
-            <Button asChild className="py-6 px-2 my-1 align-left justify-start hover:bg-black hover:text-white" variant="ghost">
-                <Link href="/" className=""> 
-                    <AiOutlineHome className="text-2xl mr-4" />
-                    <div>Home</div>
-                </Link>
-            </Button>
-            <Button asChild className="py-6 px-2 my-1 align-left justify-start hover:bg-black hover:text-white" variant="ghost">
-                <Link href="/explore" className="">
-                    <BiCategory className="text-2xl mr-4" />
-                    <div>Category</div>
-                </Link>
-            </Button>
-            <Button asChild className="py-6 px-2 my-1 align-left justify-start hover:bg-black hover:text-white" variant="ghost">
-                <Link href="/order" className="">
-                    <LuTruck className="text-2xl mr-4" />
-                    <div>Order</div>
-                </Link>
-            </Button>
-            
+            {Routes.slice(0, 3).map((route, index) => getNavigation(route))}
         </div>
         <div className="flex flex-col border-t-2 pt-2">
-            <Button asChild className="py-6 px-2 my-1 align-left justify-start hover:bg-black hover:text-white" variant="ghost">
-                <Link href="/login" className="">
-                    <FiUser className="text-2xl mr-2" />
-                    <div>Login</div>
-                </Link>
-            </Button>
-            <Button asChild className="py-6 px-2 my-1 align-left justify-start hover:bg-black hover:text-white" variant="ghost">
-                <Link href="/settings" className="">
-                    <IoSettingsOutline className="text-2xl mr-2" />
-                    <div>Settings</div>
-                </Link>
-            </Button>
+            {Routes.slice(3, 5).map((route, index) => getNavigation(route))}
         </div>
     </nav>
   );
