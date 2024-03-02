191369dea7bbbd34e6dc00cf971df03cc9448885
BebekJK <kevinjk2802@gmail.com>
2024-03-02 22:13:45 +0800

    feat: working search bar

--- src/components/searchbar/searchbar.jsx
+++ src/components/searchbar/searchbar.jsx
@@ -2,9 +2,48 @@
 import { Input } from "@/components/ui/input";
 import { GrCart } from "react-icons/gr";
 import { Button } from "@/components/ui/button";
+import { useState } from "react";
+import { IoIosSearch } from "react-icons/io";
+import productService from "@/services/product-service";
 import Link from "next/link";
 
+
+const featuredProducts = [
+    {
+        id: 4,
+        name: "Laptop 1",
+    }, 
+    {
+        id: 9,
+        name: "Phone 1",
+    },
+    {
+        id:14,
+        name: "Tabelt 1"
+    }
+]
 export default function SearchBar() {
+    const [showPanel, setShowPanel] = useState(false);
+    const [relatedSearch, setRelatedSearch] = useState(featuredProducts);
+    const [searchValue, setSearchValue] = useState("");
+
+    const filterProducts = async (search) => {
+        if(search.length === 0) {
+            setRelatedSearch(featuredProducts);
+            return;
+        }
+        setSearchValue(search);
+        const getProducts = await productService.getProducts();
+        const filteredProducts = getProducts.filter((product) => product.name.toLowerCase().includes(search.toLowerCase())).slice(0, 5);
+        setRelatedSearch(filteredProducts);
+        
+    }
+    const waitAndHide = () => {
+        setTimeout(() => {
+            setShowPanel(false);
+        }, 100);
+    }
+
     return (
         <div className="w-full flex items-center justify-center relative pt-5">
             <Link href="/explore">
@@ -16,7 +55,33 @@ export default function SearchBar() {
             <Input
                 className="ml-12 w-2/5 active:border-0"
                 placeholder="Search for products"
+                onFocus={() => setShowPanel(true)}
+                onBlur={waitAndHide}
+                onChange={(e) => filterProducts(e.target.value)}
             />
+            <div className={`absolute w-2/5 z-10 top-[4rem] border-[1px] rounded-lg border-collapse bg-primary-foreground ${showPanel? "": "hidden"}`}>
+                {
+                    relatedSearch.length === 0? <div className="pl-4 my-2">No products found matching '{searchValue}'</div>
+                    :(  
+                        <div>
+                            {
+                                relatedSearch.map((product) => {
+                                    return(
+                                        <div className="pl-4 my-2 hover:bg-muted transition-all">
+                                            <Link href={`/product/${product.id}`} className="flex w-full" onClick={waitAndHide}>
+                                                <div className="text-primary flex justify-center items-center mr-4">
+                                                    <IoIosSearch />
+                                                </div>
+                                                {product.name}
+                                            </Link>
+                                        </div>
+                                    )
+                                })
+                            }
+                        </div>
+                    )
+                }
+            </div>
             <Button
                 className="w-12 h-12 ml-2 flex items-center justify-center rounded-full"
                 variant="ghost"
@@ -28,6 +93,7 @@ export default function SearchBar() {
                     <GrCart />
                 </Link>
             </Button>
+            
         </div>
     );
 }
